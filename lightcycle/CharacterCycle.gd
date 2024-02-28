83a8dd235d161ca8210fa1cd3c15bb27c45d6548
Kirill Tregubov <contact@kirilltregubov.com>
2024-02-28 03:17:59 -0500

    updates to movement

--- lightcycle/CharacterCycle.gd
+++ lightcycle/CharacterCycle.gd
@@ -7,13 +7,33 @@ const SPEED = 250.0
 const JUMP_VELOCITY = -400.0
 
 var move_state = State.idle # State.idle
-var move_direction = Direction.down
+var move_direction = Direction.up
 @onready var player = $Sprite2D as Sprite2D
 @onready var trails = $"../Trails"
 const TRAIL_DIMENSION = 8
+const BIKE_WIDTH = 8
+const BIKE_HEIGHT = 8
 var trail = preload("res://trail/trail.tscn")
 
-var num = 0
+#var num = 0
+
+
+func calculate_direction() -> Direction:
+	var up = Input.is_action_pressed("ui_up")
+	var down = Input.is_action_pressed("ui_down")
+	var left = Input.is_action_pressed("ui_left")
+	var right = Input.is_action_pressed("ui_right")
+	
+	if up and move_direction != Direction.down:
+		if not down: return Direction.up
+	if down and move_direction != Direction.up:
+		if not up: return Direction.down
+	if left and move_direction != Direction.right:
+		if not right: return Direction.left
+	if right and move_direction != Direction.left:
+		if not left: return Direction.right
+	
+	return move_direction
 
 
 func directional_velocity(direction: Direction) -> Vector2:
@@ -47,35 +67,35 @@ func directional_rotation(direction: Direction) -> float:
 			get_tree().quit()
 			return 0
 
+func directional_position(direction: Direction) -> Vector2:
+	var cur_pos: Vector2 = self.position
+	var trail_offset = TRAIL_DIMENSION / 2
+	match direction:
+		Direction.up:
+			cur_pos.y -= player.get_rect().size.x / 2
+			pass
+		Direction.down:
+			cur_pos.y += player.get_rect().size.x / 2
+			pass
+		Direction.left:
+			cur_pos.x -= player.get_rect().size.y / 2 - trail_offset
+		Direction.right:
+			cur_pos.x += player.get_rect().size.y / 2 - trail_offset
+		_:
+			printerr("Invalid direction")
+			get_tree().quit()
+	
+	print('new pos ', cur_pos)
+	return cur_pos
+	#return Vector2(0,0)
+
 
 func _ready() -> void:
 	position.x = 640
-	position.y = 200 #500 #
+	position.y = 500 #200 #
+	set_rotation_degrees(directional_rotation(move_direction))
 	if move_state == State.moving:
 		set_velocity(directional_velocity(move_direction))
-		set_rotation_degrees(directional_rotation(move_direction))
-
-
-func calculate_direction():
-	var up = Input.is_action_pressed("ui_up")
-	var down = Input.is_action_pressed("ui_down")
-	var left = Input.is_action_pressed("ui_left")
-	var right = Input.is_action_pressed("ui_right")
-	
-	if up and move_direction != Direction.down:
-		if down: return
-		return Direction.up
-	elif down and move_direction != Direction.up:
-		if up: return
-		return Direction.down
-	elif left and move_direction != Direction.right:
-		if right: return
-		return Direction.left
-	elif right and move_direction != Direction.left:
-		if left: return
-		return Direction.right
-	
-	return
 
 
 func _physics_process(delta: float) -> void:
@@ -84,21 +104,50 @@ func _physics_process(delta: float) -> void:
 			move_state = State.moving
 			set_velocity(directional_velocity(move_direction))
 			set_rotation_degrees(directional_rotation(move_direction))
+			print(rotation_degrees)
 		return
 	elif Input.is_action_just_pressed("ui_accept"):
 		move_state = State.idle
 		return
 	
 	print('deltas ', delta, velocity, delta * velocity)
-	print(global_position)
+	print('currently at ', global_position)
 	
+	# Change direction
+	var direction = calculate_direction()
+	#if (num > 0):
+		#new_direction = Direction.right
+		#get_tree().paused = true
+		#move_and_slide()
+		#return
+	if direction != move_direction and direction is int:
+		var new_direction : Direction = direction
+		print('current ', Direction.keys()[move_direction])
+		print('new ', Direction.keys()[new_direction])
+		
+		#set_velocity(directional_velocity(new_direction))
+		#set_rotation_degrees(directional_rotation(new_direction))
+		#set_position(directional_position(new_direction))
+		
+		#set_velocity(Vector2(0,0))
+		
+		#position.y += -2
+		
+		move_direction = new_direction
+		#if move_direction == Direction.down:
+		get_tree().paused = true
+		return
+	
+	
+	# Add Trail
 	if true:
 		var change = delta * velocity
 		print('change ', change, change.abs())
+		print('size', player.get_rect().size)
 		var scaleVec = change.abs() / Vector2(TRAIL_DIMENSION, TRAIL_DIMENSION)
 		scaleVec.x = 1 if scaleVec.x == 0 else scaleVec.x
 		scaleVec.y = 1 if scaleVec.y == 0 else scaleVec.y
-		print('scale vector ', scaleVec)
+		#print('scale vector ', scaleVec)
 		var newTrail = trail.instantiate() as Node2D
 		trails.add_child(newTrail)
 		
@@ -129,25 +178,11 @@ func _physics_process(delta: float) -> void:
 		#get_tree().paused = true
 		#pass
 	
-	var new_direction = calculate_direction()
-	#if (num > 0):
-		#new_direction = Direction.right
-		#get_tree().paused = true
-		#move_and_slide()
-		#return
-	if new_direction != move_direction and new_direction is int:
-		print('current', Direction.keys()[move_direction])
-		print('new', Direction.keys()[new_direction])
-		
-		#set_velocity(directional_velocity(new_direction))
-		set_velocity(Vector2(0,0))
-		set_rotation_degrees(directional_rotation(new_direction))
-		position.y += -2
-		
-		move_direction = new_direction
-		get_tree().paused = true
-	
 	print('move')
 	move_and_slide()
-	print('end ', global_position)
-	num += 1
+	global_position = Vector2(round(global_position.x), round(global_position.y))
+	print('player at ', global_position)
+	
+	
+	#get_tree().paused = true
+	
