979d37d4f01ec1c24a979837a6c75b03f6787c96
Kirill Tregubov <contact@kirilltregubov.com>
2024-03-01 04:06:24 -0500

    cleanup comments

--- lightcycle/CharacterCycle.gd
+++ lightcycle/CharacterCycle.gd
@@ -2,18 +2,13 @@ extends CharacterBody2D
 
 enum State {idle, moving}
 
+var move_state = State.idle
+var move_direction: Constants.Direction
+const TRAIL_DIMENSION = 8
 const SPEED = 250.0
-
-var move_state = State.idle # State.idle
-var move_direction: Constants.Direction # = Constants.Direction.up
 @onready var player = $Sprite2D as Sprite2D
 @onready var trails = $"../Trails"
-const TRAIL_DIMENSION = 8
-const BIKE_WIDTH = 8
-const BIKE_HEIGHT = 8
 var trail = preload("res://trail/trail.tscn")
-
-#var num = 0
 
 
 func calculate_direction() -> Constants.Direction:
@@ -65,9 +60,9 @@ func directional_rotation(direction: Constants.Direction) -> float:
 			get_tree().quit()
 			return 0
 
+
 func directional_position(direction: Constants.Direction) -> Vector2:
 	var cur_pos: Vector2 = self.position
-	#print('stats ', player.get_rect().size)
 	match direction:
 		Constants.Direction.up:
 			cur_pos.y -= (player.get_rect().size.y - player.get_rect().size.x)/ 2
@@ -84,20 +79,16 @@ func directional_position(direction: Constants.Direction) -> Vector2:
 		_:
 			printerr("Invalid direction")
 			get_tree().quit()
-	
-	#print('new pos ', cur_pos)
 	return cur_pos
 
 
 func spawn_trail(change: Vector2, padding = Vector2(0, 0), rotate = false):
-	#print('change', change)
+	# Calculate scale
 	var scaleVec = change.abs() / Vector2(TRAIL_DIMENSION, TRAIL_DIMENSION)
 	scaleVec.x = 1 if scaleVec.x == 0 else scaleVec.x
 	scaleVec.y = 1 if scaleVec.y == 0 else scaleVec.y
 	
-	var newTrail = trail.instantiate() as Node2D
-	trails.add_child(newTrail)
-	
+	# Calculate offset
 	var offset: Vector2
 	match move_direction:
 		Constants.Direction.up:
@@ -110,18 +101,15 @@ func spawn_trail(change: Vector2, padding = Vector2(0, 0), rotate = false):
 			offset = Vector2(-TRAIL_DIMENSION/2, 0)
 		_:
 			offset = Vector2(0,0)
-	#print('offset', offset)
 	
+	# Create trail
+	var newTrail = trail.instantiate() as Node2D
+	trails.add_child(newTrail)
 	newTrail.set_global_position(global_position - change + offset + padding)
 	newTrail.set_scale(scaleVec)
 	if rotate:
 		newTrail.set_rotation_degrees(90)
-	#print('spawned at ', newTrail.global_position, newTrail.scale)
 	#get_tree().paused = true
-	#else:
-		#newTrail.queue_free()
-	#else:
-		#get_tree().paused = true
 
 
 func _ready() -> void:
@@ -134,6 +122,7 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	# Temporarily start moving
 	if move_state != State.moving:
 		if Input.is_action_just_pressed("ui_accept"):
 			move_state = State.moving
@@ -144,20 +133,10 @@ func _physics_process(delta: float) -> void:
 		move_state = State.idle
 		return
 	
-	#print('deltas ', delta, velocity, delta * velocity)
-	#print('currently at ', global_position)
-	
 	# Change direction
 	var direction = calculate_direction()
-	#if (num > 0):
-		#new_direction = Direction.right
-		#get_tree().paused = true
-		#move_and_slide()
-		#return
 	if direction != move_direction and direction is int:
 		var new_direction : Constants.Direction = direction
-		#print('current ', Constants.Direction.keys()[move_direction])
-		#print('new ', Constants.Direction.keys()[new_direction])
 		if new_direction == Constants.Direction.left or new_direction == Constants.Direction.right:
 			if move_direction == Constants.Direction.up:
 				spawn_trail(Vector2(0, -6), Vector2(0, -3))
@@ -171,17 +150,12 @@ func _physics_process(delta: float) -> void:
 		set_velocity(directional_velocity(new_direction))
 		set_rotation_degrees(directional_rotation(new_direction))
 		set_position(directional_position(new_direction))
-		
-		#if move_direction == Constants.Direction.down:
-			#get_tree().paused = true
 		move_direction = new_direction
 		return
 	
 	# Add Trail
 	spawn_trail(delta * velocity)
 	
-	#print('move')
+	# Move and snap position
 	move_and_slide()
 	global_position = Vector2(round(global_position.x), round(global_position.y))
-	#print('player at ', global_position)
-	#get_tree().paused = true
